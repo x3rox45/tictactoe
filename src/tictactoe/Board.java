42932d38a5166375d4bcc7862528deb67633e676
Max Hugen <maxhugen@hotmail.com>
2019-12-11 16:32:02 +0100

    Basic game implemented.

--- src/tictactoe/Board.java
+++ src/tictactoe/Board.java
@@ -1,32 +1,77 @@
 package tictactoe;
 
 public class Board {
-	Player player1;
-	Player player2;
-	String[][] board;
+	// constant(s)
+	private final static int BOARD_WIDTH = 3;
+	private final static int BOARD_HEIGHT = 3;
+	private final static String EMPTY = " ";
 	
-	Board(Player player1, Player player2) {
-		// initialize players
-		this.player1 = player1;
-		this.player2 = player2;
-		
-		// initialize board
-		this.board = new String[3][3];
-		for (int j = 0; j < board.length; j++) {
-			for (int i = 0; i < board[0].length; i++) {
-				board[i][j] = " ";
+	// variable initialization
+	private String[][] board;
+	
+	Board() {
+		this.board = new String[BOARD_WIDTH][BOARD_HEIGHT];
+		for (int row = 0; row < BOARD_HEIGHT; row++) {
+			for (int col = 0; col < BOARD_WIDTH; col++) {
+				this.board[row][col] = EMPTY;
 			}
 		}
 	}
 	
-	boolean isOver() {
-		for (int j = 0; j < board.length; j++) {
-			for (int i = 0; i < board[0].length; i++) {
-				if (board[i][j].equals(" ")) {
+	void placeSign(int row, int col, String sign) {
+		if (this.board[row][col].equals(EMPTY)) {
+			this.board[row][col] = sign;
+		}
+	}
+	
+	boolean isSquareEmpty(int row, int col) {
+		return (this.board[row][col].equals(EMPTY));
+	}
+	
+	boolean hasWon(String sign) {
+		// horizontal
+		for (int row = 0; row < BOARD_HEIGHT; row++) {
+			if (this.board[row][0].equals(sign) && this.board[row][1].equals(sign) && this.board[row][2].equals(sign)) {
+				return true;
+			}
+		}
+		
+		// vertical
+		for (int col = 0; col < BOARD_HEIGHT; col++) {
+			if (this.board[0][col].equals(sign) && this.board[1][col].equals(sign) && this.board[2][col].equals(sign)) {
+				return true;
+			}
+		}
+		
+		// diagonal
+		if (this.board[0][0].equals(sign) && this.board[1][1].equals(sign) && this.board[2][2].equals(sign)) {
+			return true;
+		} else if (this.board[0][2].equals(sign) && this.board[1][1].equals(sign) && this.board[2][0].equals(sign)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	boolean isFull() {
+		for (int row = 0; row < BOARD_HEIGHT; row++) {
+			for (int col = 0; col < BOARD_WIDTH; col++) {
+				if (this.board[row][col].equals(EMPTY)) {
 					return false;
 				}
 			}
 		}
 		return true;
+	}	
+	
+	void print() {
+		for (int row = 0; row < BOARD_HEIGHT; row++) {
+			for (int col = 0; col < BOARD_WIDTH; col++) {
+				System.out.printf(" " + this.board[row][col] + " ");
+				if (col != BOARD_WIDTH - 1) System.out.printf("|");
+			}
+			System.out.printf("\n");
+			if (row != BOARD_HEIGHT - 1) System.out.printf("-----------\n");
+		}
 	}
 }
--- src/tictactoe/Game.java
+++ src/tictactoe/Game.java
@@ -1,25 +1,58 @@
 package tictactoe;
 
-import java.io.PrintStream;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
 	// constant(s)
 	final static String SIGN_1 = "X";
 	final static String SIGN_2 = "O";
+	final static char[] validInputs = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	
-	// variable initialization
-	PrintStream out;
+	Player[] players;
+	int startingPlayer;
+	int currentPlayer;
+	Board board;
 	
 	Game() {
-		out = new PrintStream(System.out);
+		this.players = new Player[2];
+		this.players[0] = new Player(SIGN_1);
+		this.players[1] = new Player(SIGN_2);
+		this.startingPlayer = new Random().nextInt(players.length);
+		this.board = new Board();
+	}
+	
+	int[] readInput() {
+		int row, col, input;
+		do {
+			do {
+				System.out.printf("Where do you want to place your sign, player %d(%s)? ", currentPlayer + 1, players[currentPlayer].getSign());
+				Scanner scanner = new Scanner(System.in);
+				input = scanner.next().charAt(0);
+			} while (input < '1' || input > '9');
+			input -= '0';
+			row = 2 - ((input - 1) / 3);
+			col = (input - 1) % 3;
+		} while (!board.isSquareEmpty(row, col));
+		
+		int[] output = {row, col};
+		return output;
 	}
 	
 	private void start() {
-		Player player1 = new Player(SIGN_1);
-		Player player2 = new Player(SIGN_2);
-		Board board = new Board(player1, player2);
-		while (!board.isOver()) {
-			
+		currentPlayer = startingPlayer;
+		board.print();
+		while (!board.isFull() && !board.hasWon(players[0].getSign()) && !board.hasWon(players[1].getSign())) {
+			currentPlayer = (currentPlayer + 1) % 2;
+			int[] input = readInput();
+			board.placeSign(input[0], input[1], players[currentPlayer].getSign());
+			board.print();
+		}
+		
+		if (board.hasWon(players[currentPlayer].getSign())) {
+			System.out.printf("Game over! Player %d(%s) won!", currentPlayer + 1, players[currentPlayer].getSign());
+		} else {
+			System.out.printf("Game over! It is a tie!");
 		}
 	}
 	
